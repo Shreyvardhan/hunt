54f3fd07b422dcdefd3ee9a64c6b59dfc828246e
Akshat Srivastava <hey@aksh.at>
2014-11-09 23:06:29 +0530

    Fix wigs stuff

--- hunt/wsgi.py
+++ hunt/wsgi.py
@@ -2,7 +2,7 @@
 WSGI config for hunt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
+Doe
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
@@ -13,7 +13,7 @@ import site
 
 site.addsitedir('/home/ubuntu/.virtualenvs/hunt/lib/python2.7/site-packages')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hunt.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
